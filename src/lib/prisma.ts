abec841b275d63ab1fd040ebfa6a210a06f90946
Abdul Rahman <mrrehman002@gmail.com>
2026-04-30 01:55:07 +0500

    feat:Add cors middleware

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,14 @@
 import "dotenv/config";
+import pg from "pg";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "../generated/prisma/client.js";
 
-const connectionString = process.env.DATABASE_URL;
+const pool = new pg.Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: process.env.DATABASE_URL?.includes("rds.amazonaws.com")
+    ? { rejectUnauthorized: false }
+    : false,
+});
 
-const adapter = new PrismaPg({ connectionString });
+const adapter = new PrismaPg(pool);
 export const prisma = new PrismaClient({ adapter });
